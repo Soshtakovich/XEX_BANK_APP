413a4798604e1ef3bcf4c45788f686056e35c3a7
Soshtakovich <125446692+Soshtakovich@users.noreply.github.com>
2024-07-08 16:42:30 +0200

    Delete gunicorn_config.py

--- gunicorn_config.py
+++
@@ -1,5 +0,0 @@
-# gunicorn_config.py
-
-bind = '127.0.0.1:8000'  # Bind to localhost on port 8000 or as per your configuration
-workers = 3  # Number of worker processes
-# You can add more configurations here as needed
