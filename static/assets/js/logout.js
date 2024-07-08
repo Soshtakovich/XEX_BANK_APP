6f90a925828a22f4f80a5f0e3f661c5fb123e94d
Soshtakovich <125446692+Soshtakovich@users.noreply.github.com>
2024-07-08 19:36:48 +0200

    Update logout.js

--- static/assets/js/logout.js
+++ static/assets/js/logout.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Add click event listener to the logout button
-    document.getElementById('logoutBtn').addEventListener('click', function () {
-        // Perform logout actions
-        sessionStorage.removeItem('user_id'); // Clear the user ID from session storage
-        sessionStorage.removeItem('user_name'); // Clear the user name from session storage (if needed)
-        sessionStorage.removeItem('user_surname'); // Clear the user surname from session storage (if needed)
 
-        // Redirect or perform any other logout operations as needed
-        console.log('User logged out');
-        // Example: Redirect to index.html
+    document.getElementById('logoutBtn').addEventListener('click', function () {
+  
+        sessionStorage.removeItem('user_id'); 
+        sessionStorage.removeItem('user_name'); 
+        sessionStorage.removeItem('user_surname'); 
+
+        #console.log('User logged out');
+
         window.location.href = "/index_2";
     });
 });
